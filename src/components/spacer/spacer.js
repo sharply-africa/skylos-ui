a83973d7695f2e6554f488d9adec721ee59ff0e5
Remilekun Salami <remilekun.salami@gmail.com>
2021-02-26 01:10:00 +0100

    feat: spacer

---
+++ src/components/spacer/spacer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { space } from "styled-system";
+
+const classnames = (...args) => args.join(" ");
+const getClassName = (el) => (el.props && el.props.className) || "";
+
+export const StyledChildren = ({ className, children, ...props }) => {
+  const styledChildren = React.Children.toArray(children).map((child) =>
+    React.cloneElement(child, {
+      className: classnames(getClassName(child), className),
+    })
+  );
+  return <>{styledChildren}</>;
+};
+
+const Spacer = styled(StyledChildren)(space);
+
+export { Spacer };
