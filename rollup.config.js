e5b227648f66727a3df595981dadb3db18577ca4
Remilekun Salami <remilekun.salami@gmail.com>
2021-02-28 13:16:53 +0100

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
+import path from "path";
 import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import image from "@rollup/plugin-image";
+import visualizer from "rollup-plugin-visualizer";
+import alias from "@rollup/plugin-alias";
 import del from "rollup-plugin-delete";
+import svgr from "@svgr/rollup";
 import pkg from "./package.json";
+
+const projectRootDir = path.resolve(__dirname);
+
+const srcDir = path.resolve(projectRootDir, "src");
 
 export default {
   input: pkg.source,
@@ -13,11 +23,24 @@ export default {
   ],
   plugins: [
     external(),
+    alias({
+      entries: [
+        { find: "components", replacement: `${srcDir}/components` },
+        { find: "css-reset", replacement: `${srcDir}/css-reset` },
+        { find: "icons", replacement: `${srcDir}/icons` },
+        { find: "system", replacement: `${srcDir}/system` },
+        { find: "theme", replacement: `${srcDir}/theme` },
+      ],
+    }),
+    svgr(),
     babel({
       exclude: "node_modules/**",
     }),
+    nodeResolve(),
+    image(),
     del({ targets: ["dist/*"] }),
     commonjs(),
     terser(),
+    visualizer(),
   ],
 };
