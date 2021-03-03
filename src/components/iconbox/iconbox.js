ce7f5972b8fbb08e8a29c7ea0f50a8daca132059
Remilekun Salami <remilekun.salami@gmail.com>
2021-03-03 21:52:07 +0100

    chore: rewrite iconbox

--- src/components/iconbox/iconbox.js
+++ src/components/iconbox/iconbox.js
@@ -11,16 +11,18 @@ const Wrapper = styled(Flex)`
 export const IconBox = forwardRef((props, ref) => {
   return (
     <Wrapper
-      alignItems="center"
-      backgroundColor="primaryLight"
-      borderRadius="base"
-      height="6.6rem"
-      justifyContent="center"
-      px={2}
-      py={2}
-      width="6.6rem"
       ref={ref}
       {...props}
+      __css={{
+        alignItems: "center",
+        backgroundColor: "primaryLight",
+        borderRadius: "base",
+        height: "6.6rem",
+        justifyContent: "center",
+        px: 2,
+        py: 2,
+        width: "6.6rem",
+      }}
     />
   );
 });
