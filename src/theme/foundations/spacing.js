37dc2684957a7cb1afcdcab6149afcbf4a208b4e
Remilekun Salami <remilekun.salami@gmail.com>
2021-10-19 22:05:03 +0100

    v1.0.0

--- src/theme/foundations/spacing.js
+++ src/theme/foundations/spacing.js
@@ -1,10 +1,8 @@
 export const spacing = [...new Array(96)].reduce((acc, _, x) => {
   acc = {
     ...acc,
-    [x]: x * 4,
-    [`${x}px`]: x,
-    [`-${x}`]: -(x * 4),
-    [`-${x}px`]: -x,
+    [x]: `${(x * 4) / 10}rem`,
+    [`-${x}`]: `${-(x * 4) / 10}rem`,
   };
   return acc;
 }, {});
