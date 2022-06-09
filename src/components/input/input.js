d46a8e4c07dbe40362e83eb0377ec2073c461981
Remilekun Salami <remilekun.salami@gmail.com>
2022-06-09 11:51:48 +0100

    fix: input address type autocomplete regions

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -91,107 +91,109 @@ const InputComponent = forwardRef((props, ref) => (
 
 const AUTOCOMPLETE_FIELDS = ["geometry.location", "formatted_address"];
 
-export const Input = forwardRef(({ onChange, type, value, ...props }, ref) => {
-  const hasSetDefaultValue = React.useRef(false);
-  const { googleMapsKey } = useSkylosTheme();
+export const Input = forwardRef(
+  ({ onChange, type, value, placesOptions, ...props }, ref) => {
+    const hasSetDefaultValue = React.useRef(false);
+    const { googleMapsKey } = useSkylosTheme();
 
-  const { ref: placesRef } = usePlacesWidget({
-    apiKey: googleMapsKey,
-    onPlaceSelected: (location) => {
-      placesRef.current.value = location?.formatted_address || "";
-      onChange(location);
-    },
-    options: {
-      types: ["geocode", "establishment"],
-      componentRestrictions: { country: "ng" },
-      fields: AUTOCOMPLETE_FIELDS,
-    },
-  });
+    const { ref: placesRef } = usePlacesWidget({
+      apiKey: googleMapsKey,
+      onPlaceSelected: (location) => {
+        placesRef.current.value = location?.formatted_address || "";
+        onChange(location);
+      },
+      options: {
+        types: ["geocode", "establishment"],
+        fields: AUTOCOMPLETE_FIELDS,
+        ...placesOptions,
+      },
+    });
 
-  const {
-    placePredictions,
-    getPlacePredictions,
-    placesService,
-    isPlacePredictionsLoading,
-  } = usePlacesService({
-    apiKey: googleMapsKey,
-    options: {
-      types: ["address"],
-      componentRestrictions: { country: "ng" },
-    },
-  });
+    const {
+      placePredictions,
+      getPlacePredictions,
+      placesService,
+      isPlacePredictionsLoading,
+    } = usePlacesService({
+      apiKey: googleMapsKey,
+      options: {
+        types: ["address"],
+        ...placesOptions,
+      },
+    });
 
-  React.useEffect(() => {
-    if (
-      props.defaultValue &&
-      type === "address" &&
-      !hasSetDefaultValue.current
-    ) {
-      placesRef.current.value = props.defaultValue;
-      getPlacePredictions({ input: props.defaultValue });
-      hasSetDefaultValue.current = true;
-    }
-  }, [props.defaultValue, type]);
+    React.useEffect(() => {
+      if (
+        props.defaultValue &&
+        type === "address" &&
+        !hasSetDefaultValue.current
+      ) {
+        placesRef.current.value = props.defaultValue;
+        getPlacePredictions({ input: props.defaultValue });
+        hasSetDefaultValue.current = true;
+      }
+    }, [props.defaultValue, type]);
 
-  React.useEffect(() => {
-    if (!isPlacePredictionsLoading && placePredictions.length) {
-      const { place_id } = placePredictions[0];
+    React.useEffect(() => {
+      if (!isPlacePredictionsLoading && placePredictions.length) {
+        const { place_id } = placePredictions[0];
 
-      placesService?.getDetails(
-        { placeId: place_id, fields: AUTOCOMPLETE_FIELDS },
-        (place) => {
-          onChange(place);
-          hasSetDefaultValue.current = false;
-        }
+        placesService?.getDetails(
+          { placeId: place_id, fields: AUTOCOMPLETE_FIELDS },
+          (place) => {
+            onChange(place);
+            hasSetDefaultValue.current = false;
+          }
+        );
+      }
+    }, [isPlacePredictionsLoading, placePredictions, placesService, onChange]);
+
+    React.useEffect(() => {
+      if (!value && placesRef?.current) {
+        placesRef.current.value = "";
+      }
+    }, [placesRef, value]);
+
+    if (type === "phone") {
+      return (
+        <PhoneInputWrapper ref={ref}>
+          <PhoneInput
+            autoFormat
+            country="ng"
+            inputProps={{ ...props }}
+            onChange={onChange}
+            value={value}
+            {...props}
+          />
+        </PhoneInputWrapper>
       );
     }
-  }, [isPlacePredictionsLoading, placePredictions, placesService, onChange]);
 
-  React.useEffect(() => {
-    if (!value && placesRef?.current) {
-      placesRef.current.value = "";
-    }
-  }, [placesRef, value]);
-
-  if (type === "phone") {
-    return (
-      <PhoneInputWrapper ref={ref}>
-        <PhoneInput
-          autoFormat
-          country="ng"
-          inputProps={{ ...props }}
-          onChange={onChange}
-          value={value}
+    if (type === "address") {
+      return (
+        <InputComponent
+          ref={mergeRefs(ref, placesRef)}
+          autoComplete="off"
+          type={type}
           {...props}
+          onBlur={(evt) => {
+            props.onBlur?.(evt);
+            if (!evt.target.value) {
+              onChange(null);
+            }
+          }}
         />
-      </PhoneInputWrapper>
-    );
-  }
+      );
+    }
 
-  if (type === "address") {
     return (
       <InputComponent
-        ref={mergeRefs(ref, placesRef)}
-        autoComplete="off"
+        onChange={onChange}
+        ref={ref}
         type={type}
+        value={value}
         {...props}
-        onBlur={(evt) => {
-          props.onBlur?.(evt);
-          if (!evt.target.value) {
-            onChange(null);
-          }
-        }}
       />
     );
   }
-
-  return (
-    <InputComponent
-      onChange={onChange}
-      ref={ref}
-      type={type}
-      value={value}
-      {...props}
-    />
-  );
-});
+);
